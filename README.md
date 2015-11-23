21a0e3493fed4cce531b9c05daaf0a5ee667bfc0
nmaekawa <nmaekawa@g.harvard.edu>
2015-11-23 11:45:54 -0500

    skeleton for python epiphan api client

---
+++ README.md
@@ -0,0 +1,22 @@
+epipearl
+===============================
+
+python client for epiphan-pearl device api
+
+* Free software: Apache 2.0 license
+
+Features
+--------
+
+* TODO
+
+Credits
+---------
+
+Tools used in rendering this package:
+
+*  Cookiecutter_
+*  `cookiecutter-pypackage`_
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ tests/test_epipearl.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_epipearl
+----------------------------------
+
+Tests for `epipearl` module.
+"""
+
+import unittest
+
+from epipearl import epipearl
+
+
+class TestEpipearl(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
